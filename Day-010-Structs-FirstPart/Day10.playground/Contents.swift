7fb619cc164baff8699bf9b0fbcb067bccea5697
Peralta24 <rafamtz301203@gmail.com>
2025-09-30 16:34:54 -0600

    Primer Struct del dia 10

--- Day-010-Structs-FirstPart/Day10.playground/Contents.swift
+++ Day-010-Structs-FirstPart/Day10.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Dia 10 de 100 dias con Swift
+
+struct Album {
+    let title : String
+    let artis : String
+    let year : Int
+    
+    func printSummary() {
+        print("Album: \(title)")
+        print("Artista: \(artis)")
+        print("Año: \(year)")
+    }
+}
+
+
+var album1 = Album(title: "Thriller", artis: "Michael Jackson", year: 1982)
+album1.printSummary()
+print(album1.title)
+print(album1.artis)
+
+
+
+struct Employee {
+    let name : String
+    var vacationRemaining : Int
+    
+    mutating func takeVacation(days : Int) {
+        if vacationRemaining >= days {
+            vacationRemaining -= days
+            print("Im goin on vacation")
+            print("Days remaining : \(vacationRemaining)")
+        }else {
+            print("Oops! There arent enough days remaining.")
+        }
+    }
+}
+var archer = Employee(name : "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 3)
+archer.takeVacation(days: 11)
+print(archer.vacationRemaining)
