384e1e80278d9781a8e33e841884db04055a8e3b
Peralta24 <rafamtz301203@gmail.com>
2025-10-02 07:53:24 -0600

    Acabando la teoria de static metodos y valores

--- Day-011-Structs-PartTwo/Day11.playground/Contents.swift
+++ Day-011-Structs-PartTwo/Day11.playground/Contents.swift
@@ -30,3 +30,65 @@ if succes {
     print("Failed to get the money")
 }
 
+//Propiedades y metodos estaticos
+
+struct School {
+    static var studentCount = 0
+    
+    static func add (student : String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+School.add(student: "Taylor Swift")
+School.add(student: "Ed Sheeran")
+print("Total students: \(School.studentCount)")
+
+//Usar propiedas staticas parea organizar datos comunes en mis aplicaciones
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFileName = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+print(AppData.version)
+print(AppData.saveFileName)
+print(AppData.homeURL)
+
+
+//Crear ejemplos de mis estructuras con static
+
+struct EmEmployee {
+    let username : String
+    let password : String
+    
+    static let example = EmEmployee(username: "Jim1Poggers", password: "A0r1GLn4IP45sW0RD")
+}
+
+print(EmEmployee.example)
+
+struct Unwrap {
+    static let appURL = "https://itunes.apple.com/app/id1440611372"
+    //Entropia ajustada
+
+    static var entropy = Int.random(in: 1...1000)
+
+    static func getEntropy() -> Int {
+        entropy += 1
+        return entropy
+    }
+
+}
+
+Unwrap.getEntropy()
+
+struct LegoBrick {
+    static var numberMade = 0
+    var shape : String
+    var color : String
+    init(shape : String, color: String){
+        self.shape = shape
+        self.color = color
+        LegoBrick.numberMade += 1
+    }
+}
