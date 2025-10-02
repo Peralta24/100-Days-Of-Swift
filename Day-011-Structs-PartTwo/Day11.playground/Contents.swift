ecb7e6f34394b67e1fd109a01b3b5ce0b0e65209
Peralta24 <rafamtz301203@gmail.com>
2025-10-02 07:04:38 -0600

    Acabando el primier tema de Teoria deL dia 10

--- Day-011-Structs-PartTwo/Day11.playground/Contents.swift
+++ Day-011-Structs-PartTwo/Day11.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Dia 11 de 100 dias con Swift
+
+struct BankAccount {
+    
+    private var funds = 0
+    
+    mutating func deposit(_ amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(_ amount: Int) -> Bool {
+        if amount >= funds {
+            funds -= amount
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(100)
+let succes = account.withdraw(200)
+
+if succes {
+    print("Withdrew money succesfully")
+}else{
+    print("Failed to get the money")
+}
+
