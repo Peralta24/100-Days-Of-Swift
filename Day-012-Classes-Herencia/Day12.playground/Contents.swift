a3b8187d0984d6a49c759a9f078d36d71a7d2874
Peralta24 <rafamtz301203@gmail.com>
2025-10-02 14:55:54 -0600

    Primer tema teorico del dia 12 de 100 dias con SwiftUi

--- Day-012-Classes-Herencia/Day12.playground/Contents.swift
+++ Day-012-Classes-Herencia/Day12.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Day 12 de 100 dias con SwiftUI
+
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score: \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score = 10
+newGame.score = 20 
