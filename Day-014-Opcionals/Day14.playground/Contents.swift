24ec42dc7ae2c7c7bd26d2728231690ef64bb13e
Peralta24 <rafamtz301203@gmail.com>
2025-10-04 07:22:31 -0600

    Acabando la teoria de opciones del dia 14

--- Day-014-Opcionals/Day14.playground/Contents.swift
+++ Day-014-Opcionals/Day14.playground/Contents.swift
@@ -2,3 +2,59 @@ import UIKit
 
 //Dia 14 de 100 dias con Swift
 //Opcionales
+
+let opposites = [
+    "Mario":"Luigi",
+    "Luigi" :"Waluigi"
+]
+let peachOpposite = opposites["Peach"]
+
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+
+var username : String? = nil
+username = "Jose"
+
+if let unwrappedUsername = username {
+    print("Hello \(unwrappedUsername)")
+} else {
+    print("Hello, world!")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number : Int? = nil
+number = 5
+if let number = number {
+    print(square(number: number))
+}else{
+    print("NO HAY UN NUMERO")
+}
+
+func getUsername() -> String? {
+    nil
+}
+
+if let username = getUsername() {
+    print("Username is \(username)")
+}else{
+    print("No user name")
+}
+var bestScore: Int? = nil
+bestScore = 101
+
+if let bestScore {
+    if bestScore > 100 {
+        print("You got a high score!")
+    } else {
+        print("Better luck next time.")
+    }
+}
+
+//Ejerciicos primera leccion opcionales
+
