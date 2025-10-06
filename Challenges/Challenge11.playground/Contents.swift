5b978f6ebe6e364c81d754d0d23774166ddad295
Peralta24 <rafamtz301203@gmail.com>
2025-10-05 19:59:44 -0600

    Acabando el ejercicio numero 1 del challenge 11

--- Challenges/Challenge11.playground/Contents.swift
+++ Challenges/Challenge11.playground/Contents.swift
@@ -3,4 +3,40 @@ import UIKit
 //Ejercicio repaso dia 1 - 14
 //App de gastos personales Ejercicio 1
 
+// 1.- Crear struct Gasto
+struct Gasto {
+    let nombre : String
+    var monto : Double
+    let categoria : String
+    var descripcion : String {
+        return("El nombre es \(nombre) con el monto de \(monto) y categoria \(categoria)")
+    }
+}
 
+let gastos = [
+    Gasto(nombre: "Café", monto: 45, categoria: "Comida"),
+    Gasto(nombre: "Uber", monto: 120, categoria: "Transporte"),
+    Gasto(nombre: "Netflix", monto: 139, categoria: "Entretenimiento")
+]
+var sumaComida = 0.0
+var sumaEntretenimiento = 0.0
+var sumaTransporte = 0.0
+var montosCategorias : [String : Double] = [:]
+for gasto in gastos {
+    
+    if gasto.categoria == "Comida" {
+        sumaComida += gasto.monto
+        montosCategorias["Comida"] = sumaComida
+    }else if gasto.categoria == "Entretenimiento" {
+        sumaEntretenimiento += gasto.monto
+        montosCategorias["Entretenimiento"] = sumaEntretenimiento
+    }else if gasto.categoria == "Transporte" {
+        sumaTransporte += gasto.monto
+        montosCategorias["Transporte"] = sumaTransporte
+    }
+}
+print("La suma total de comida es \(sumaComida)")
+print("La suma total de transporte es \(sumaTransporte)")
+print("La suma total de entretenimiento es \(sumaEntretenimiento)")
+
+print(montosCategorias)
