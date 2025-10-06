3e3b7f782a1a8d27b401a959d2e17e0c1190494b
Peralta24 <rafamtz301203@gmail.com>
2025-10-06 16:14:47 -0600

    Tap Count con @State

--- Project-01/WeSplit/WeSplit/ContentView.swift
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -5,18 +5,21 @@
 //  Created by Jose Rafael Peralta Martinez  on 06/10/25.
 //
 //probar git
-
+//Ejecutar el simulador Cmd+R
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Button("Tap Count: \(tapCount)") {
+                tapCount += 1
+            }
+            .navigationTitle(Text("Tap Count"))
+            .navigationBarTitleDisplayMode(.large)
         }
-        .padding()
+
     }
 }
 
