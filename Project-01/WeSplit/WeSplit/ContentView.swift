ac53a7fb851ef9b6af1f0838497bd406bde2626f
Peralta24 <rafamtz301203@gmail.com>
2025-10-07 09:38:18 -0600

    Agregando la seccion de Tip percentage?

--- Project-01/WeSplit/WeSplit/ContentView.swift
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -28,6 +28,15 @@ struct ContentView: View {
                         }
                     }
                 }
+                Section("How much do you want to tip?"){
+                    
+                    Picker("Tip percentege",selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
                 Section{
                     Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
@@ -37,8 +46,6 @@ struct ContentView: View {
 
     }
 }
-
-
 #Preview {
     ContentView()
 }
