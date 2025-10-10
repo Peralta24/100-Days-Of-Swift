4de859cfb8f232b837e6fc331efc67a4620d3170
Peralta24 <rafamtz301203@gmail.com>
2025-10-09 21:21:21 -0600

    Agregando la actividad de Practica

--- Project-03/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project-03/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,18 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
-    
+    @State private var mensaje = "Selecciona una accion"
+    @State private var alerta = false
     var body: some View {
-        Button("Show Alert"){
-            showingAlert = true
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.orange, .pink]), startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea()
+            
+            VStack(spacing:30){
+                Text("Panel de Control")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                
+                
+                Text(mensaje)
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+
+                HStack(spacing:50){
+                    Image(systemName: "play")
+                    Image(systemName: "pause")
+                    Image(systemName: "stop")
+                }
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                
+                
+                HStack(spacing: 20){
+                   
+                    Button("Accion1"){
+                        mensaje = "Accion 1 activada!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.green)
+                    
+                    
+                    Button("Accion2",role: .destructive){
+                        mensaje = "Accion 2 activada!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button("Alerta"){
+                        alerta = true
+                    }
+                    .padding()
+                    .foregroundStyle(.white)
+                    .buttonStyle(.borderedProminent)
+                    .alert("Importante",isPresented: $alerta){
+                        Button("Save"){}
+                        Button("Undo"){}
+                    }message: {
+                        Text("Mensaje Importante")
+                    }
+
+                }
+                Label("Configuracion", systemImage: "gear")
+                    .padding()
+                    .background(.cyan)
+                    .cornerRadius(12)
+            }
+
+            
         }
-        .alert("Important message", isPresented: $showingAlert){
-            Button("Delete",role:.destructive){}
-            Button("Cancel",role: .cancel){}
-        }message: {
-            Text("Please read this important message")
-        }
+        
+
     }
 
 }
