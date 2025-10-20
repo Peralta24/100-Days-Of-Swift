b8bd33b4a9dbf5d32d62f155ae4815f1e295d383
Peralta24 <rafamtz301203@gmail.com>
2025-10-20 14:13:45 -0600

    Acabando primera teoira del proyeto 5

--- Project-05/WordScramble/WordScramble/ContentView.swift
+++ Project-05/WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    var numeros = [1,2,3,4,5,6,8,9,10]
+    @State private var genteCercana = ["Mama","Papa","Hermana","Novia"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section ("SECTION 1 ") {
+                Text("Static Row 1")
+                Text("Static Row 2")
+            }
+            
+            Section("SECTION 2"){
+                ForEach(0..<5){
+                    Text("Dynamic Row \($0)")
+                }
+            }
+            
+            Section("SECTION 3"){
+                Text("Static Row 3")
+                Text("Static Row 4")
+            }
         }
-        .padding()
+        .listStyle(.plain)
+        List(numeros, id: \.self){
+            Button("Button number \(String($0))",action: agregarPersona)
+        }
+        .listStyle(.sidebar)
+        List(genteCercana, id:\.self){
+            Text("Persona cercanas: \($0)")
+            
+            ForEach(genteCercana,id: \.self){
+                Text("Personal Cercana: \($0)")
+            }
+        }
+        .listStyle(.plain)
+        
+    }
+    
+    func agregarPersona(){
+        genteCercana.append("Nueva persona agregada")
     }
 }
 
