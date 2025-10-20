ff6ea74db75149e1c56945711592a8bf0266834a
Peralta24 <rafamtz301203@gmail.com>
2025-10-20 14:46:44 -0600

    Cargando recuerso de un paquete

--- Project-05/WordScramble/WordScramble/ContentView.swift
+++ Project-05/WordScramble/WordScramble/ContentView.swift
@@ -8,47 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var numeros = [1,2,3,4,5,6,8,9,10]
-    @State private var genteCercana = ["Mama","Papa","Hermana","Novia"]
     var body: some View {
-        List {
-            Section ("SECTION 1 ") {
-                Text("Static Row 1")
-                Text("Static Row 2")
-            }
-            
-            Section("SECTION 2"){
-                ForEach(0..<5){
-                    Text("Dynamic Row \($0)")
-                }
-            }
-            
-            Section("SECTION 3"){
-                Text("Static Row 3")
-                Text("Static Row 4")
-            }
-        }
-        .listStyle(.plain)
-        List(numeros, id: \.self){
-            Button("Button number \(String($0))",action: agregarPersona)
-        }
-        .listStyle(.sidebar)
-        List(genteCercana, id:\.self){
-            Text("Persona cercanas: \($0)")
-            
-            ForEach(genteCercana,id: \.self){
-                Text("Personal Cercana: \($0)")
-            }
-        }
-        .listStyle(.plain)
+        Text("Hello")
+    }
         
-    }
-    
-    func agregarPersona(){
-        genteCercana.append("Nueva persona agregada")
-    }
 }
-
+func getUrl(){
+    if let url = Bundle.main.url(forResource: "images", withExtension: "jpg")
+        if let content = try? String(contentsOf: url){
+        //We loades the file in to a string
+    }
+        
+}
 #Preview {
     ContentView()
 }
+
