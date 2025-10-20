ea51cb5629e89ab027b7e9e06e0ad12309d36859
Peralta24 <rafamtz301203@gmail.com>
2025-10-20 13:29:46 -0600

    Empezando el proyecto numero cinco

---
+++ Project-05/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jose Rafael Peralta Martinez  on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
