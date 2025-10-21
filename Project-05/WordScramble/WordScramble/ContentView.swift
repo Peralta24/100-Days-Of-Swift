ce837447b0ae03a17c8ab03f7846ed74e684b6d7
Peralta24 <rafamtz301203@gmail.com>
2025-10-21 05:29:55 -0600

    Empezando el dia 2 del proyecto

--- Project-05/WordScramble/WordScramble/ContentView.swift
+++ Project-05/WordScramble/WordScramble/ContentView.swift
@@ -1,26 +1,13 @@
-//
-//  ContentView.swift
-//  WordScramble
-//
-//  Created by Jose Rafael Peralta Martinez  on 20/10/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
-    }
         
-}
-func getUrl(){
-    if let url = Bundle.main.url(forResource: "images", withExtension: "jpg")
-        if let content = try? String(contentsOf: url){
-        //We loades the file in to a string
+        Text("Hello, World!")
+ 
     }
-        
 }
+
 #Preview {
     ContentView()
 }
-
