3b327c72956ec24b6f8525054309f349467db960
Peralta24 <rafamtz301203@gmail.com>
2025-10-22 07:51:40 -0600

    Default animation

--- Project-06/Animation/Animation/ContentView.swift
+++ Project-06/Animation/Animation/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmoun = 1.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap me"){
+                animationAmoun += 0.1
+            
         }
-        .padding()
+            .padding(50)
+            .background(animationAmoun > 3 ? Color.green : Color.red)
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmoun)
+            .blur(radius: (animationAmoun - 1) * 3)
+            .animation(.default, value: animationAmoun)
     }
 }
 
