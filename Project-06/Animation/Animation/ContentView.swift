68717ed274b8062466ce6aa9a416e477830268d6
Peralta24 <rafamtz301203@gmail.com>
2025-10-22 08:52:48 -0600

    Animaciones de enlace y personalizadas

--- Project-06/Animation/Animation/ContentView.swift
+++ Project-06/Animation/Animation/ContentView.swift
@@ -9,18 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmoun = 1.0
+    @State private var animation2 = 1.0
+    @State private var cupsOfTee = 1.0
     var body: some View {
-        Button("Tap me"){
-                animationAmoun += 0.1
+        
+        VStack{
+            Text("Hello, World!")
+            Stepper("How many tee\(cupsOfTee)",value: $cupsOfTee.animation(), in: 0...5,step: 2)
+            
+            Button("\(Image(systemName: "tree"))"){
+                cupsOfTee += 0.2
+            }
+            .padding(2)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .scaleEffect(CGFloat(cupsOfTee))
+
+        
             
         }
+        VStack{
+            Stepper("Scale amount",value: $animation2.animation(.easeIn), in: 1...5)
+            Spacer()
+            
+            Button("Tap me"){
+                animation2 += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .scaleEffect(CGFloat(animation2))
+        }
+        VStack{
+            Button("Tap me"){
+                //animationAmoun += 1
+                
+            }
             .padding(50)
             .background(animationAmoun > 3 ? Color.green : Color.red)
             .foregroundStyle(.white)
             .clipShape(Circle())
-            .scaleEffect(animationAmoun)
-            .blur(radius: (animationAmoun - 1) * 3)
-            .animation(.default, value: animationAmoun)
+            
+            .overlay(
+                Circle()
+                    .stroke(Color.blue)
+                    .scaleEffect(animationAmoun)
+                    .animation(
+                        .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: animationAmoun
+                    )
+                
+            )
+            .onAppear {
+                animationAmoun = 2
+            }
+        }
+            
     }
 }
 
