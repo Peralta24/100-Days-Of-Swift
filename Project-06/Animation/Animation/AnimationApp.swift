6adde0062670edfc87755ddbceacb9cc4e2f7da1
Peralta24 <rafamtz301203@gmail.com>
2025-10-22 07:23:40 -0600

    Empezando la creacion del proyecto 06

---
+++ Project-06/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Jose Rafael Peralta Martinez  on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project-06/Animation/AnimationTests/AnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTests.swift
+//  AnimationTests
+//
+//  Created by Jose Rafael Peralta Martinez  on 22/10/25.
+//
+
+import Testing
+@testable import Animation
+
+struct AnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
