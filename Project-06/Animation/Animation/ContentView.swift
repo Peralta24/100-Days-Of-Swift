d93864b2a3074ef335e1e1f314f6a643703841ed
Peralta24 <rafamtz301203@gmail.com>
2025-10-23 06:27:01 -0600

    Animacion con gestos mas dinamicas

--- Project-06/Animation/Animation/ContentView.swift
+++ Project-06/Animation/Animation/ContentView.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+let letters = Array("Hello, World!")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     var body: some View {
-        
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomLeading)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius:  10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged{ dragAmount = $0.translation}
-                    .onEnded { _ in
-                        withAnimation(.bouncy){
-                            dragAmount = .zero }
-                    }
-            )
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self){letter in
+                Text(String(letters[letter]))
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(letter)/20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged{dragAmount = $0.translation}
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
         
     }
 }
