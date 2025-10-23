f2ad298608a2ca8f226d0cab0ea247b44538fe7c
Peralta24 <rafamtz301203@gmail.com>
2025-10-23 07:45:07 -0600

    Transiciones personalizadas

--- Project-06/Animation/Animation/ContentView.swift
+++ Project-06/Animation/Animation/ContentView.swift
@@ -6,7 +6,21 @@
 //
 
 import SwiftUI
-
+struct CornerRotateModifier : ViewModifier {
+    let amount : Double
+    let anchor : UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+extension AnyTransition{
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
 struct ContentView: View {
 let letters = Array("Hello, World!")
     @State private var enabled = false
@@ -64,7 +78,23 @@ let letters = Array("Hello, World!")
                 }
         )
         
-        
+        ZStack{
+            Rectangle()
+                .fill(.purple)
+                .frame(width: 100, height: 100)
+            
+            if isShowingGreen {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingGreen.toggle()
+            }
+        }
         
     }
 }
