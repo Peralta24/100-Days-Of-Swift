a4668b7a104588432124f9070e96187d02b8161a
Peralta24 <rafamtz301203@gmail.com>
2025-10-24 06:20:17 -0600

    Generando las preguntas segun dificultad

--- Proyectos-Servilleta/Edutainment/Edutainment/ContentView.swift
+++ Proyectos-Servilleta/Edutainment/Edutainment/ContentView.swift
@@ -16,24 +16,54 @@ struct ContentView: View {
     @State private var selectTable = 2
     @State private var selectNumberOfQuestions = 5
     @State private var numberOfQuestions = [5,10,20]
+    @State private var question = [String]()
     var body: some View {
-        Form {
-            //Seccion de eleccion de tabla
-            Section("Select the table you wanna practice"){
-                Stepper("Table of \(selectTable)",value: $selectTable, in: 2...12)
-            }
-            //Seccion de eleccion de preguntas
-            Section("Select how many questions do you want"){
-                Picker("Number of questions", selection: $selectNumberOfQuestions){
-                    ForEach(numberOfQuestions, id: \.self){ number in
-                        Text("\(number)")
+        NavigationStack{
+            
+            Form {
+                //Seccion de eleccion de tabla
+                Section("Select the table you wanna practice"){
+                    Stepper("Table of \(selectTable)",value: $selectTable, in: 2...12)
+                }
+                //Seccion de eleccion de preguntas
+                Section("Select how many questions do you want"){
+                    Picker("Number of questions", selection: $selectNumberOfQuestions){
+                        ForEach(numberOfQuestions, id: \.self){ number in
+                            Text("\(number)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Button("Show me the questions"){
+                        generateRandomQuestion(difficult: selectNumberOfQuestions, table: selectTable)
+                    }
+                    List(question,id:\.self){question in
+                        Text(question)
                     }
                 }
-                .pickerStyle(.segmented)
+            }
+            .navigationTitle(Text("Edutainment"))
+            .toolbar{
+                Button("Reset"){
+                    question.removeAll()
+                }
             }
         }
     }
     
+    
+    func generateRandomQuestion(difficult: Int, table: Int) {
+        
+        
+        for _ in 1...difficult{
+            _ = table
+            let randomNumber2: Int = Int.random(in: 2...12)
+            question.append("\(table) x \(randomNumber2)")
+        }
+        
+    }
 
 }
 
