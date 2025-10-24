ee02d9dd845a2a9ee1c0d10f6c1b8fe11f3a138b
Peralta24 <rafamtz301203@gmail.com>
2025-10-24 05:31:40 -0600

    Empezando endutainment

---
+++ Proyectos-Servilleta/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Jose Rafael Peralta Martinez  on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
