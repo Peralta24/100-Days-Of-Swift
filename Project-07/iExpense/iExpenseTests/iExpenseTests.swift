fbb65759ef613e25b9601b5c8097f1d9e1b548cb
Peralta24 <rafamtz301203@gmail.com>
2025-10-27 05:23:47 -0600

    Empezando con la creacion del proyecto

---
+++ Project-07/iExpense/iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by Jose Rafael Peralta Martinez  on 27/10/25.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
