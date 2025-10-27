40276431bf54fb8772d61368c6d03a0478edfe30
Peralta24 <rafamtz301203@gmail.com>
2025-10-27 05:55:48 -0600

    Diferencia entre struct y en class con @State

--- Project-07/iExpense/iExpense/ContentView.swift
+++ Project-07/iExpense/iExpense/ContentView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+@Observable
+class User {
+    var firstName = "Jose"
+    var lastName = "Peralta"
+}
 
 struct ContentView: View {
+    @State private var user = User()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            TextField("First Name",text: $user.firstName)
+            TextField("Last Name",text: $user.lastName)
         }
         .padding()
     }
