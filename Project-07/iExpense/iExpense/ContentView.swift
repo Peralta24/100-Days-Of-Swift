b10dc18ca790bc7f046afa5876841a0e8e169765
Peralta24 <rafamtz301203@gmail.com>
2025-10-27 06:29:58 -0600

    Usando obeservable, y sheets

--- Project-07/iExpense/iExpense/ContentView.swift
+++ Project-07/iExpense/iExpense/ContentView.swift
@@ -12,14 +12,42 @@ class User {
     var lastName = "Peralta"
 }
 
+@Observable
+class Numero {
+    var contador = 0
+    
+    func aumentar () {
+        contador += 1
+    }
+}
+struct secondView : View {
+    var name : String
+    var body: some View {
+        Text("Hello to the second view \(name)")
+    }
+}
 struct ContentView: View {
     @State private var user = User()
+    @State private var numero = Numero()
+    @State private var showSecondView = false
 
     var body: some View {
         VStack {
             Text("Your name is \(user.firstName) \(user.lastName)")
             TextField("First Name",text: $user.firstName)
             TextField("Last Name",text: $user.lastName)
+            
+            Text("Contador \(numero.contador)")
+            Button("Aumentar"){
+                numero.aumentar()
+            }
+            
+            Button("Go to the second View"){
+                showSecondView.toggle()
+            }
+        }
+        .sheet(isPresented: $showSecondView){
+            secondView(name : "\(user.firstName) \(user.lastName)")
         }
         .padding()
     }
