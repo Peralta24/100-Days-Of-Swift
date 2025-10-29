2452bfb2bfb288f0ec8f9e8f4336dc00f3a04327
Peralta24 <rafamtz301203@gmail.com>
2025-10-29 17:41:47 -0600

    Creando la clase

--- Project-07/iExpense/iExpense/ContentView.swift
+++ Project-07/iExpense/iExpense/ContentView.swift
@@ -1,1 +1,20 @@
 
+import SwiftUI
+
+//Crear la estructura
+struct Gasto : Identifiable, Codable{
+    var id =  UUID()
+    let nombre : String
+    let categoria : String
+    let monto : Double
+}
+//Crear la clase 
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
