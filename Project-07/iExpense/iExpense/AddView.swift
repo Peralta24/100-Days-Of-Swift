c11ca78e253f625d2ed15d362c841b1658e7c99b
Peralta24 <rafamtz301203@gmail.com>
2025-10-29 17:58:52 -0600

    Terminando la practica

--- Project-07/iExpense/iExpense/AddView.swift
+++ Project-07/iExpense/iExpense/AddView.swift
@@ -1,1 +1,37 @@
 
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    var gastos = Gastos()
+    
+    @State private var nombre = ""
+    @State private var categoria = "Personal"
+    @State private var cantidad = 0.0
+    let categorias = ["Personal", "Negocio"]
+    var body: some View {
+        NavigationStack {
+            Form{
+                TextField("Nombre", text: $nombre)
+                Picker("Categoría", selection: $categoria) {
+                    ForEach(categorias, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Cantidad", value: $cantidad, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+            .navigationTitle(Text("Añadir Gasto"))
+            .toolbar {
+                Button("Guardar"){
+                    let nuevoGasto = Gasto(nombre: nombre, categoria: categoria, monto: cantidad)
+                    gastos.index.append(nuevoGasto)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView()
+}
--- Project-07/iExpense/iExpense/ContentView.swift
+++ Project-07/iExpense/iExpense/ContentView.swift
@@ -8,10 +8,83 @@ struct Gasto : Identifiable, Codable{
     let categoria : String
     let monto : Double
 }
-//Crear la clase 
+//Crear la clase
+@Observable
+class Gastos{
+    var index = [Gasto]()
+}
 struct ContentView: View {
+    @State private var gastos = Gastos()
+    var gastosPersonales : [Gasto] {
+        gastos.index.filter{$0.categoria == "Personal"}
+    }
+    var gastosNegocios : [Gasto] {
+        gastos.index.filter{$0.categoria == "Negocio"}
+    }
+    @State private var mostrarAgregar = false
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack{
+            List{
+                Section("Gastos Personales"){
+                    ForEach(gastosPersonales){gasto in
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text(gasto.nombre)
+                                    .font(.headline)
+                                
+                                Text(gasto.categoria)
+                            }
+                            Spacer()
+                            Text(gasto.monto.formatted())
+                                .foregroundStyle(gasto.monto < 10 ? .green : gasto.monto < 100 ? .yellow : .red)
+                        }
+                    }
+                    .onDelete(perform: borrarGastosPersonal)
+                }
+                Section("Gastos Negocios"){
+                    ForEach(gastosNegocios){gasto in
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text(gasto.nombre)
+                                    .font(.headline)
+                                
+                                Text(gasto.categoria)
+                            }
+                            Spacer()
+                            Text(gasto.monto.formatted())
+                                .foregroundStyle(gasto.monto < 10 ? .green : gasto.monto < 100 ? .yellow : .red)
+                        }
+                    }
+                    .onDelete(perform: borrarGastosNegocio)
+                }
+            }
+            .navigationTitle(Text("Gastos"))
+            .toolbar{
+                Button("Agregar",systemImage: "plus"){
+                    mostrarAgregar = true
+                }
+            }
+            .sheet(isPresented: $mostrarAgregar){
+                AddView(gastos: gastos)
+            }
+        }
+    }
+    func borrarGastosPersonal(offset: IndexSet) {
+        // Obtén los elementos personales actuales
+        let personales = gastos.index.enumerated().filter { $0.element.categoria == "Personal" }
+        
+        // Convierte los índices de gastosPersonales en índices del arreglo original
+        let indicesAEliminar = offset.map { personales[$0].offset }
+        
+        // Borra esos elementos del arreglo original
+        gastos.index.remove(atOffsets: IndexSet(indicesAEliminar))
+    }
+    func borrarGastosNegocio(offset: IndexSet) {
+        let negocios = gastos.index.enumerated().filter { $0.element.categoria == "Negocio" }
+        
+        let indicesAEliminar = offset.map { negocios[$0].offset }
+        
+        gastos.index.remove(atOffsets: IndexSet(indicesAEliminar))
     }
 }
 
