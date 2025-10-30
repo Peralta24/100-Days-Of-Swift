9ce82fa7ced8673001aa528980ff7eeceaca66e9
Peralta24 <rafamtz301203@gmail.com>
2025-10-30 07:07:02 -0600

    NavigationLink

--- Project-08/Moonshot/Moonshot/ContentView.swift
+++ Project-08/Moonshot/Moonshot/ContentView.swift
@@ -2,24 +2,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(.example)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal, alignment: .center){size, axis in
-                size * 0.8
-            }
-        
-        ScrollView {
-            LazyVStack {
-                ForEach(0..<100){
-                    Text("\($0)")
+        NavigationStack{
+            Image(.example)
+                .resizable()
+                .scaledToFit()
+                .containerRelativeFrame(.horizontal, alignment: .center){size, axis in
+                    size * 0.8
+                }
+            
+            ScrollView {
+                LazyVStack {
+                    ForEach(0..<100){
+                        Text("\($0)")
+                    }
                 }
             }
-        }
-        ScrollView(.horizontal){
-            LazyHStack{
-                ForEach(0..<100){
-                    Text("\($0)")
+            ScrollView(.horizontal){
+                LazyHStack{
+                    ForEach(0..<100){
+                        Text("\($0)")
+                    }
+                }
+            }
+            NavigationLink{
+                Color.black
+            } label : {
+                VStack{
+                    Text("Presioname")
+                    Text(":)")
+                    Image(systemName: "chevron.right")
+                        .font(.title)
+                }
+            }
+            
+            List(0..<21){filas in
+                NavigationLink("Filas \(filas)"){
+                    Text("Detalles")
                 }
             }
         }
