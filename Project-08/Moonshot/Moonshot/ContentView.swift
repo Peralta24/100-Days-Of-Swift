e0e93e2c3e6be7c2580ddecba2fe702b07cd19c5
Peralta24 <rafamtz301203@gmail.com>
2025-11-01 22:35:03 -0600

    Practicando

--- Project-08/Moonshot/Moonshot/ContentView.swift
+++ Project-08/Moonshot/Moonshot/ContentView.swift
@@ -1,124 +1,47 @@
 import SwiftUI
 
-struct Usuarios : Codable {
-    let name : String
-    let address: Address
-}
-
-struct Address : Codable {
-    let street : String
-    let city : String
-}
 struct ContentView: View {
-    let frame = [
-        GridItem(.adaptive(minimum: 100))
-    ]
-    
     var body: some View {
-        NavigationStack {
-            
-            VStack {
-                Image(.example)
-                    .resizable()
-                    .scaledToFit()
-                    .scaledToFill()
-                    .frame(width: 200, height: 200)
-                    .scaledToFit()
-                
-            }
-            .navigationTitle(Text("Example"))
-            VStack{
-                NavigationLink("Scroll Vertical") {
-                    ScrollView{
-                            LazyVStack{
-                                ForEach(0..<100){numero in
-                                    Text("\(numero)")
+        NavigationStack{
+            VStack(spacing:20){
+                VStack(alignment: .leading){
+                    Image(.example)
+                        .resizable()
+                        .frame(width: 380, height: 200)
+                        .scaledToFill()
+                }
+                VStack(alignment: .leading){
+                    NavigationLink("Ir al scroll vertical"){
+                        ScrollView{
+                            LazyVStack(spacing:20){
+                                ForEach(0..<300){num in
+                                    Text("Numero \(num)")
+                                }
                             }
                         }
                     }
                 }
-                NavigationLink("Scroll Horizontal"){
-                    ScrollView(.horizontal){
-                        LazyHStack{
-                            ForEach(0..<100){numero in
-                                Text("\(numero)")
+                VStack(alignment: .leading){
+                    NavigationLink("Ir al scroll horizontal"){
+                        ScrollView(.horizontal){
+                            LazyHStack{                            ForEach(0..<300){num in
+                                Text("Numero \(num)")
+                            }
                             }
                         }
                     }
                 }
-                NavigationLink{
-                    Text("Bienvenido")
-                }label:{
-                    Text("Presioname")
-                }
-                
-                NavigationLink("Vista de mensajes"){
-                    List(0..<21){fila in
-                        NavigationLink("Filas \(fila)"){
-                            Text("Detalles")
-                        }
-                    }
-                }
-                NavigationLink("Vista de distintas Opciones"){
-                    List(0..<30){fila in
-                        NavigationLink("Opcion \(fila)"){
-                            Text("Detalle opcion \(fila)")
-                        }
-                    }
-                }
-                
-                Button("Decode JSON"){
-                    let input = """
-                                                    {
-                                                        "name": "Taylor Swift",
-                                                        "address": {
-                                                            "street": "555, Taylor Swift Avenue",
-                                                            "city": "Nashville"
-                                                        }
-                                                    }
-                                                    """
-                    
-                    let data = Data(input.utf8)
-                    if let datos = try? JSONDecoder().decode(Usuarios.self, from: data){
-                        print(datos.address.city)
-                        print(datos.name)
+                VStack(alignment: .leading){
+                    NavigationLink("Ir a los mensajes"){
                         
-                    }
-                    
-                    
-                    
-                }
-                
-                NavigationLink("Vista de grid vertical"){
-                    ScrollView{
-                        LazyVGrid(columns: frame){
-                            ForEach(0..<200){numero in
-                                Text("\(numero)")
-                            }
-                        }
-                    }
-                }
-                NavigationLink("Vista de grid horizontal"){
-                    ScrollView(.horizontal){
-                        LazyHGrid(rows: frame){
-                            ForEach(0..<200){numero in
-                                Text("\(numero)")
-                            }
-                        }
-                    }
-                }
-                
-                NavigationLink("Practica de lista con enlace"){
-                    List(0..<21){opcion in
-                        NavigationLink("Opcion \(opcion)"){
-                            Text("Detalle opcion \(opcion)")
-                        }
                     }
                 }
             }
         }
+            
     }
 }
+
 
 #Preview {
     ContentView()
