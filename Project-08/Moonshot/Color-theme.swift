cb1b2219e968dc82b863779bb9584957e450b565
Peralta24 <rafamtz301203@gmail.com>
2025-11-03 20:23:27 -0600

    Acabando

---
+++ Project-08/Moonshot/Color-theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color-theme.swift
+//  Moonshot
+//
+//  Created by Jose Rafael Peralta Martinez  on 03/11/25.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    
+    static var darkBackground : Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground : Color {
+        Color(red: 0.3, green: 0.3, blue: 0.3)
+    }
+}
