f336ce88bf5dd73ce6fbc01b94e49dabf4cfa79c
Peralta24 <rafamtz301203@gmail.com>
2025-11-21 19:10:28 -0600

    f

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Jose Rafael Peralta Martinez  on 21/11/25.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String?
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+    
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName ?? "Unknown album")
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=nf&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
+            results = decodedResponse.results
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jose Rafael Peralta Martinez  on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
