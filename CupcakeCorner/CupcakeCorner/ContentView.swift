5536eb49bb4a44681fb465b8ce89aee8e79662a2
Peralta24 <rafamtz301203@gmail.com>
2025-11-22 16:37:38 -0600

    Trabajando con vibraciones

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -6,26 +6,50 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
-@Observable
-class User: Codable {
-    enum CodingKeys: String, CodingKey {
-        case _name = "name"
-    }
-    var name = "Taylor"
-}
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var engine : CHHapticEngine?
     var body: some View {
-        Button("Encode Taylor",action: encodeTaylor)
+        Button("Tap Me",action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+        .sensoryFeedback(.increase, trigger: tapCount)
     }
     
-    func encodeTaylor(){
-        let data = try! JSONEncoder().encode(User())
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+    func prepareHaptics(){
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {return}
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+        }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
-}
+    }
 
 #Preview {
     ContentView()
