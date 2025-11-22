839b61edfe310b31e9e88e10b69bd64dc3485b93
Peralta24 <rafamtz301203@gmail.com>
2025-11-22 14:06:34 -0600

    Aprendiendo a trabajr con codbale el @observaler

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,88 +7,23 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String?
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Taylor"
 }
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    @State private var userName = ""
-    @State private var email = ""
-    
-    var isDisabled: Bool {
-        userName.count < 5 || email.count < 5
-    }
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName ?? "Unknown album")
-                
-                
-            }
-        }
-        AsyncImage(url: URL(string : "https://hws.dev/img/logo.png"),scale: 12)
-        
-        AsyncImage(url: URL(string:"https://hws.dev/img/logo.png")){image in
-                image
-                .resizable()
-                .scaledToFit()
-        }placeholder: {
-            ProgressView()
-        }
-        
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            }else if phase.error != nil {
-                Text("There was an error loading the image")
-            }else {
-                ProgressView()
-            }
-        }
-        .frame(width: 200,height: 200)
-
-        Form {
-            Section {
-                TextField("User name", text: $userName)
-                TextField("Email", text: $email)
-            }
-            Section {
-                Button("Create account"){
-                    print("Creating acount")
-                }
-            }
-            .disabled(isDisabled)
-        }
-        .task {
-            await loadData()
-        }
+        Button("Encode Taylor",action: encodeTaylor)
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=nf&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
-            results = decodedResponse.results
-        } catch {
-            print("Error: \(error)")
-        }
+    func encodeTaylor(){
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
     }
 }
 
