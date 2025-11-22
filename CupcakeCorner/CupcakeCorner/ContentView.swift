1315b76e24320f21648c677a327435e54b7324e9
Peralta24 <rafamtz301203@gmail.com>
2025-11-22 13:32:46 -0600

    volviendo

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,92 +8,88 @@
 import SwiftUI
 
 struct Response: Codable {
-    var results : [Result]
+    var results: [Result]
 }
 
-struct Result : Codable {
-    var trackId : Int
-    var trackName : String
-    var collectionName : String
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String?
 }
-
 
 struct ContentView: View {
- @State private  var canciones = [Result]()
-    @State private var nameUser = ""
-    @State private var emailUser = ""
-     private var isDisable : Bool {
-        nameUser.isEmpty || emailUser.isEmpty
+    @State private var results = [Result]()
+    @State private var userName = ""
+    @State private var email = ""
+    
+    var isDisabled: Bool {
+        userName.count < 5 || email.count < 5
     }
-var body: some View {
-        NavigationStack {
-            VStack{
-                List(canciones, id: \.trackId){ cancion in
-                        Text(cancion.trackName)
-                        .font(.headline)
-                    
-                    Text(cancion.collectionName)
-                        .font(.caption)
-                }
-            }
-            
-            .task {
-                await loadSongs()
-            }
-            
-            VStack{
-                TextField("Nombre", text: $nameUser)
-                TextField("Email", text: $emailUser)
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
                 
-                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"),scale: 12)
+                Text(item.collectionName ?? "Unknown album")
                 
-                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){image in
-                    image
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                }placeholder: {
-                    ProgressView()
-                }
                 
-                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){phase in
-                    if let image = phase.image {
-                        image
-                            .resizable()
-                            .scaledToFit()
-                    }else if phase.error != nil {
-                        Text("There was an error loading the image")
-                    }else {
-                        ProgressView()
-                    }
-                }
             }
-            VStack{
-                Button("Enviar"){
-                    print("Enviar \(nameUser) \(emailUser)")
-                }
-            }
-            .disabled(isDisable)
-            
-            .navigationTitle("Canciones NF")
         }
-    
+        AsyncImage(url: URL(string : "https://hws.dev/img/logo.png"),scale: 12)
+        
+        AsyncImage(url: URL(string:"https://hws.dev/img/logo.png")){image in
+                image
+                .resizable()
+                .scaledToFit()
+        }placeholder: {
+            ProgressView()
+        }
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            }else if phase.error != nil {
+                Text("There was an error loading the image")
+            }else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200,height: 200)
+
+        Form {
+            Section {
+                TextField("User name", text: $userName)
+                TextField("Email", text: $email)
+            }
+            Section {
+                Button("Create account"){
+                    print("Creating acount")
+                }
+            }
+            .disabled(isDisabled)
+        }
+        .task {
+            await loadData()
+        }
     }
     
-    func loadSongs() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=nf&entity=song&limit=20")else {
-            print("Error to load the songs")
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=nf&entity=song") else {
+            print("Invalid URL")
             return
         }
         
         do {
-                    let (data, _) = try await URLSession.shared.data(from: url)
-                    let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
-            canciones = decodedResponse.results
-                } catch {
-                    print("Error: \(error)")
-                }
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedResponse = try JSONDecoder().decode(Response.self, from: data)
+            results = decodedResponse.results
+        } catch {
+            print("Error: \(error)")
+        }
     }
-    
 }
 
 #Preview {
