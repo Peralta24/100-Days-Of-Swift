5416db8c5a8944bf15486cfb0c07b7ebb521611c
Peralta24 <rafamtz301203@gmail.com>
2025-11-30 08:20:44 -0600

    Leccion 1

---
+++ Project-12/SwiftDataProject/SwiftDataProject/EditUserView.swift
@@ -0,0 +1,32 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Jose Rafael Peralta Martinez  on 30/11/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct EditUserView: View {
+    @Bindable var user: User
+    var body: some View {
+        Form {
+            TextField("Name",text: $user.name)
+            TextField("City",text: $user.city)
+            DatePicker("Join Date", selection: $user.joinDate)
+        }
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: User.self, configurations: config)
+        let user = User(name: "Taylor Swift", city: "Nashville", joinDate: .now)
+        return EditUserView(user: user)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create container: \(error.localizedDescription)")
+    }
+}
---
+++ Project-12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Jose Rafael Peralta Martinez  on 30/11/25.
+//
+
+import SwiftData
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: User.self)
+    }
+}
